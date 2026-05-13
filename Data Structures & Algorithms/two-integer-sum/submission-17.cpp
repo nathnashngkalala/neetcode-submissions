dea9c1d87de8c6d55ea8eac1e5c3c683725c74c0
nathnashngkalala <124426687+nathnashngkalala@users.noreply.github.com>
2026-05-13 17:33:20 +0800

    Add: two-integer-sum - submission-17

---
+++ Data Structures & Algorithms/two-integer-sum/submission-17.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /*
+        so we want to return indicies that reaches the target
+        brute force is n^2 and checke every pair but if we 
+        know each number index based on value if we see the other
+        we can count the complement if it exist we have the answer
+        this is a O(n) solution, so the integers arent unique which is interesting
+        means that the first can have the same value we can just go trough it right
+        once we do if i != j is enough to check, so we just record the smallest index?
+    */
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> indexMap;
+        for(int i = 0; i < nums.size(); ++i) {
+            int x = nums[i];
+            if(!indexMap.count(x)) {
+                indexMap[x] = i;
+            }
+        }
+        for(int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if(indexMap.count(complement) && indexMap[complement] != i) {
+                return {min(i, indexMap[complement]), max(i, indexMap[complement])};
+            }
+        }
+        return {-1,-1};
+    }
+};
